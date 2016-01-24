69abe1638383d34d0a36e95cc324db7f3a50cbdc
Young <young@Youngs-MacBook-Air.local>
2016-01-25 00:27:56 +0700

    all features will be available in june 2016

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ var login = require('login');
 var userlist = require('user-list');
 var postlist = require('post-list');
 var home = require('home');
+var error = require('error');
 
 
 app.use(login);
 app.use(userlist);
 app.use(postlist);
 app.use(home);
+app.use(error);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.env.PORT || 5000, function(){
     console.log('The app is running on port 3002!!');
 });
+
