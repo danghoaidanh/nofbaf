015a71841349dacd7a078ff930013fcd8c75d95c
Young <young@Youngs-MacBook-Air.local>
2016-01-24 16:59:39 +0700

    this is testing

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+
+var login = require('login');
+var userlist = require('user-list');
+var postlist = require('post-list');
+var home = require('home');
+
+
+app.use(login);
+app.use(userlist);
+app.use(postlist);
+app.use(home);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3002, function(){
+    console.log('The app is running on port 3002!!');
+});
