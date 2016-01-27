5e7f66edda6788484a72f826d8bc28c7d1a418d3
Young <young@Youngs-MacBook-Air.local>
2016-01-27 22:32:58 +0700

    thing

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var postlist = require('post-list');
 var home = require('home');
 var error = require('error');
 var signup = require('signup');
+var homein = require('homein');
 
 
 app.use(login);
@@ -17,6 +18,7 @@ app.use(postlist);
 app.use(home);
 app.use(error);
 app.use(signup);
+app.use(homein);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.env.PORT || 5000, function(){
